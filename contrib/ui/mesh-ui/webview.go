781cb7b9f6bf43a6a67f1aa6263e58d82ff3c59b
Vadym Vikulin <vadym.vikulin@gmail.com>
2022-04-26 21:27:01 +0300

    Update webview.go

--- contrib/ui/mesh-ui/webview.go
+++ contrib/ui/mesh-ui/webview.go
@@ -144,9 +144,9 @@ func run(w webview.WebView){
 
 func run_command(riv_ctrl_path string, command string) []byte{
 	args := []string{"-json", command}
-	//cmd := exec.Command(riv_ctrl_path, args...)
-	exec.Command(riv_ctrl_path, args...)
+	cmd := exec.Command(riv_ctrl_path, args...)
 	//out, err := cmd.CombinedOutput()
+	cmd.CombinedOutput()
 	//if err != nil {
 	//	log.Fatalf("cmd.Run() failed with %s\n", err)
 	//	return nil
