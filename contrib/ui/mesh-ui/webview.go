2c03226610b968c0bc776be35b42af8d72b117ba
Vadym Vikulin <vadym.vikulin@gmail.com>
2022-04-26 23:09:26 +0300

    Update webview.go

--- contrib/ui/mesh-ui/webview.go
+++ contrib/ui/mesh-ui/webview.go
@@ -102,9 +102,9 @@ func main() {
     w.Bind("ping", func(peer_list string) {
         go ping(w, peer_list)
     })
-    //dat, err := ioutil.ReadFile(path+"/index.html")
-    //w.Navigate("data:text/html,"+url.QueryEscape(string(dat)))
-    w.Navigate("data:text/html,"+"<html>"+path+"</html>")
+    dat, err := ioutil.ReadFile(path+"/index.html")
+    w.Navigate("data:text/html,"+url.QueryEscape(string(dat)))
+    //w.Navigate("data:text/html,"+"<html>"+path+"</html>")
     w.Run()
 }
 
