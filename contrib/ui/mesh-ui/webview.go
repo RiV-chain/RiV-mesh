e5e67db4796662b57691f3c312cbb57a776a94d3
Vadym Vikulin <vadym.vikulin@gmail.com>
2022-04-27 00:05:57 +0300

    Update webview.go

--- contrib/ui/mesh-ui/webview.go
+++ contrib/ui/mesh-ui/webview.go
@@ -152,28 +152,28 @@ func get_user_home_path() string {
     }
 }
 
-func get_ctl_path() string{
+func get_ctl_path() string[]{
     if runtime.GOOS == "windows" {
 		program_path := "programfiles"
 		path, exists := os.LookupEnv(program_path)
 		if exists {
 			fmt.Println("Program path: %s", path)
 			riv_ctrl_path := fmt.Sprintf("%s\\RiV-mesh\\meshctl.exe", path)
-			return riv_ctrl_path
+			return [3]string{"cmd.exe", "/c", riv_ctrl_path}
 		} else {
 			fmt.Println("could not find Program Files path")
-            return ""
+			return []string{}
 		}
 	} else {
 		riv_ctrl_path := fmt.Sprintf("meshctl")
-		return riv_ctrl_path
+		return return [3]string{"bash", "-c", riv_ctrl_path}
 	}
 }
 
 func run(w webview.WebView){
     //var peersTimer *time.Timer
     riv_ctrl_path := get_ctl_path()
-    if riv_ctrl_path != "" {
+    if len(riv_ctrl_path) > 0 {
         get_self(w, riv_ctrl_path)
 	get_peers(w, riv_ctrl_path)
     }
@@ -183,8 +183,9 @@ func run(w webview.WebView){
 }
 
 func run_command(riv_ctrl_path string, command string) []byte{
-	args := []string{"-json", command}
-	cmd := exec.Command(riv_ctrl_path, args...)
+	//args := []string{"-json", command}
+	riv_ctrl_path[2] = riv_ctrl_path[2]+" -json "+command
+	cmd := exec.Command(riv_ctrl_path)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		//log.Fatalf("cmd.Run() failed with %s\n", err)
@@ -194,8 +195,9 @@ func run_command(riv_ctrl_path string, command string) []byte{
 }
 
 func run_command_with_arg(riv_ctrl_path string, command string, arg string) []byte{
-	args := []string{"-json", command, arg}
-	cmd := exec.Command(riv_ctrl_path, args...)
+	//args := []string{"-json", command, arg}
+	riv_ctrl_path[2] = riv_ctrl_path[2]+" -json "+command
+	cmd := exec.Command(riv_ctrl_path)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
         	//log.Fatalf("command failed: %s\n", riv_ctrl_path+" "+strings.Join(args, " "))
