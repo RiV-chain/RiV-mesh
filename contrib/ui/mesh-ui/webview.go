32888ec9b4740d33f1510b75975f856ffd007252
Vadym Vikulin <vadym.vikulin@gmail.com>
2022-04-26 21:12:43 +0300

    Update webview.go

--- contrib/ui/mesh-ui/webview.go
+++ contrib/ui/mesh-ui/webview.go
@@ -52,7 +52,16 @@ func main() {
        	if err := hjson.Unmarshal(conf, &dat); err != nil {
         	fmt.Printf("Unable to parse mesh.conf file: %v", err)
         } else {
-            
+            if dat["Peers"]!=nil {
+                peers := dat["Peers"].([]interface{}) 
+                remove_peers()
+                //for _, u := range peers {
+                //   log.Printf("Unmarshaled: %v", u.(string))
+                //   add_peers(u.(string))
+                //}
+            } else {
+                fmt.Printf("Warning: Peers array not loaded from mesh.conf file")
+            }
         }
     }
     w.Run()
