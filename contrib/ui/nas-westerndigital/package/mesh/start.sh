2979028cd6df36ef7d048e618a1560e47ce50216
vadym <vadym.vikulin@gmail.com>
2022-05-07 14:45:44 +0300

    1. install path var fix

--- contrib/ui/nas-westerndigital/package/mesh/start.sh
+++ contrib/ui/nas-westerndigital/package/mesh/start.sh
@@ -3,9 +3,6 @@
 MESH_PACKAGE_LOG=/var/log/mesh.log
 echo "start.sh called" >> "$MESH_PACKAGE_LOG"
 
-ln -fs "$INST_PATH"/apache-mesh.conf /usr/local/apache2/conf/extra
-( sleep 2 ; /usr/sbin/apache restart web ) &
-
 pid=`pidof -s mesh`
 if [ -n "$pid" ]; then
   echo "start.sh: mesh already running" >> "$MESH_PACKAGE_LOG"
@@ -16,6 +13,9 @@ EXE_PATH=`readlink -f /usr/bin/mesh`
 BIN_PATH=`dirname $EXE_PATH`
 INSTALL_PATH=`dirname $BIN_PATH`
 config_file=$INSTALL_PATH/mesh.conf
+
+ln -fs "$INST_PATH"/apache-mesh.conf /usr/local/apache2/conf/extra
+( sleep 2 ; /usr/sbin/apache restart web ) &
 
 START_COMMAND="/usr/bin/mesh -useconffile '$config_file' -httpaddress 'http://localhost:19019' -wwwroot '${INSTALL_PATH}/www' -logto '${INSTALL_PATH}/var/log/mesh.log'"
 echo "start.sh: starting (START_COMMAND=$START_COMMAND)" >> "$MESH_PACKAGE_LOG"
