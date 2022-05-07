6253e1fd106d9b6f6a15a6c06ff0a0d66e03d9d4
vadym <vadym.vikulin@gmail.com>
2022-05-07 08:47:13 +0300

    1. symlink fix

--- contrib/ui/nas-westerndigital/package/mesh/install.sh
+++ contrib/ui/nas-westerndigital/package/mesh/install.sh
@@ -10,6 +10,6 @@ path_dst="$2"
 rm -rf "$INST_PATH"
 mv "$path_src" "$path_dst"
 #LOG_SYMLINK
-ln -s "$INST_PATH"/www/log "$INST_PATH"/var/log/mesh.log
-#ln -s "$INST_PATH"/apache-mesh.conf /usr/local/apache2/conf/extra
+ln -s "$INST_PATH"/var/log/mesh.log "$INST_PATH"/www/log
+ln -s "$INST_PATH"/apache-mesh.conf /usr/local/apache2/conf/extra
 ( sleep 2 ; /usr/sbin/apache restart web ) &
