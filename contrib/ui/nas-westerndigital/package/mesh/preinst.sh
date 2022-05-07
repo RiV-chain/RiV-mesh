fa0128b2cc394c930e3e818c4790a5ddfee5197f
vadym <vadym.vikulin@gmail.com>
2022-05-07 12:36:57 +0300

    1. added more logs

--- contrib/ui/nas-westerndigital/package/mesh/preinst.sh
+++ contrib/ui/nas-westerndigital/package/mesh/preinst.sh
@@ -4,6 +4,9 @@
 MESH_PACKAGE_LOG=/var/log/mesh-preinst.log
 echo "preinst.sh called" >> "$MESH_PACKAGE_LOG"
 
+exec 2>>"$MESH_PACKAGE_LOG"
+set -x
+
 path_dst="$1"
 echo "path_dst=$path_dst" >> "$MESH_PACKAGE_LOG"
 
