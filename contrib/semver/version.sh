65615bb5aafc26fee7c35648b41183d611dbaaf7
Vadym Vikulin <vadym.vikulin@rivchain.org>
2024-08-29 19:14:17 +0300

    Version fix

--- contrib/semver/version.sh
+++ contrib/semver/version.sh
@@ -3,7 +3,7 @@
 case "$*" in
   *--bare*)
     # Remove the "v" prefix
-    git describe --abbrev=0 --tags --match="[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*"
+    git describe --abbrev=0 --tags --match="v[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | sed 's/^v//'
     ;;
   *)
     git describe --abbrev=0 --tags --match="v[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*"
